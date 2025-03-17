8c1fb9a4be11cadeb29d80d3c1434381195fcc3a
geunu-kim <splendid0904@hufs.ac.kr>
2025-03-17 15:31:43 +0900

    Title: 알파벳에 따른 평가, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250317/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,15 @@
+C = str(input())
+
+if C == "S" :
+    print("Superior")
+elif C == "A" :
+    print("Excellent")
+elif C == "B" :
+    print("Good")
+elif C == "C" :
+    print("Usually")
+elif C == "D" :
+    print("Effort")
+else :
+    print("Failure")
+
