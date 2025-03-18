e63a8f53e50266ee18c0b6002961f21d11e237e5
geunu-kim <splendid0904@hufs.ac.kr>
2025-03-18 18:23:15 +0900

    Title: 두 번의 연산, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250318/두 번의 연산/two-operations.py
+++ 250318/두 번의 연산/two-operations.py
@@ -1,5 +1,9 @@
 A = int(input())
 
+if A % 2 == 0 :
+    A /= 3
+    print(int(A))
+
 if A % 2 == 1 :
     A += 3
     if A % 3 == 0 :
@@ -7,7 +11,3 @@ if A % 2 == 1 :
         print(int(A))
     else :
         print(int(A))
-
-else :
-    A /= 3
-    print(int(A))
