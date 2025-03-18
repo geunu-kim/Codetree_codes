0722224bae013f26f22e5104f3bef645b30570b3
geunu-kim <splendid0904@hufs.ac.kr>
2025-03-18 18:32:27 +0900

    Title: 두 수의 짝홀 여부, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250318/두 수의 짝홀 여부/parity-of-two-numbers.py
@@ -0,0 +1,15 @@
+A = input().split()
+
+a = int(A[0])
+b = int(A[1])
+
+if a % 2 == 0 :
+    print("even")
+else :
+    print("odd")
+
+if b % 2 == 0 :
+    print("even")
+else :
+    print("odd")
+
