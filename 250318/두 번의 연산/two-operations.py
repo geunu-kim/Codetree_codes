61b3a473f1b5fddb35344d7778c2821f22125913
geunu-kim <splendid0904@hufs.ac.kr>
2025-03-18 18:28:07 +0900

    Title: 두 번의 연산, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250318/두 번의 연산/two-operations.py
+++ 250318/두 번의 연산/two-operations.py
@@ -1,13 +1,17 @@
 A = int(input())
 
-if A % 2 == 0 :
-    A /= 3
-    print(int(A))
-
 if A % 2 == 1 :
     A += 3
     if A % 3 == 0 :
         A /= 3
+        print(int(A))
+    else :
+        print(int(A))
+else :
+    if A % 3 == 0 :
+        A /= 3
+        print(int(A))
+    else :
         print(int(A))
 
 
