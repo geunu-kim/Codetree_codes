0351f60463579e071e3ee22aed57b959846d2afc
geunu-kim <splendid0904@hufs.ac.kr>
2025-03-18 18:44:16 +0900

    Title: 비교 연산, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250318/비교 연산/comparison-operator.py
@@ -0,0 +1,34 @@
+D = input().split()
+
+A = int(D[0])
+B = int(D[1])
+
+if A >= B :
+    print("1")
+else :
+    print("0")
+
+if A > B :
+    print("1")
+else :
+    print("0")
+
+if B >= A :
+    print("1")
+else :
+    print("0")
+
+if B > A :
+    print("1")
+else :
+    print("0")
+
+if A == B :
+    print("1")
+else :
+    print("0")
+
+if A != B :
+    print("1")
+else :
+    print("0") 
