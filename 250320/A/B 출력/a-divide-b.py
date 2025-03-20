265409e8c5bfda71d8cbb62d1dc265398d4d55e7
geunu-kim <splendid0904@hufs.ac.kr>
2025-03-20 15:32:56 +0900

    Title: A/B 출력, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/A/B 출력/a-divide-b.py
+++ 250320/A/B 출력/a-divide-b.py
@@ -1,6 +1,13 @@
 arr = input().split()
 a = int(arr[0]) ; b = int(arr[1])
-c = float(a / b)
 
-for i in range (21) :
-    
+print(f"{a//b}.", end="")
+
+a %= b
+
+for _ in range (20) :
+    a *= 10
+    print(a//b, end="")
+
+    a %= b
+
