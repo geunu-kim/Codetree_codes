96eacca47751a44ab449523eaf76822e2361491f
geunu-kim <splendid0904@hufs.ac.kr>
2025-03-20 16:45:27 +0900

    Title: 높은 수에서 낮은 수까지, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250320/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,11 @@
+S = input().split()
+A = int(S[0]) ; B = int(S[1])
+
+if A > B :
+    for _ in range (A, B-1, -1) :
+        print(A, end= " ")
+        A -= 1
+elif A < B :
+    for _ in range (B, A-1, -1) :
+        print(B, end=" ")
+        B -= 1
