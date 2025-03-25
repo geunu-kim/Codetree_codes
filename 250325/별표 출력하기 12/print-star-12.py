336b04b99f03e5798b322bdd9f86cffb6ee414ff
geunu-kim <splendid0904@hufs.ac.kr>
2025-03-25 14:18:51 +0900

    Title: 별표 출력하기 12, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250325/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+for i in range (n) :
+    if i == 0 :
+        for j in range (n) :
+            print("* ",end="")
+        print()
+    else :
+        for j in range (n) :
+            if j % 2 == 0 :
+                print("  ",end="")
+            else: 
+                if i <= j :
+                    print("* ",end="")
+                else :
+                    print("  ",end="")
+        print()
+
+
+
+
