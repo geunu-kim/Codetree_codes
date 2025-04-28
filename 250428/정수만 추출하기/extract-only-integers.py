8f8311460d7bca4fbbdb35004808ca7fb1d68526
geunu-kim <splendid0904@hufs.ac.kr>
2025-04-28 18:20:04 +0900

    Title: 정수만 추출하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250428/정수만 추출하기/extract-only-integers.py
+++ 250428/정수만 추출하기/extract-only-integers.py
@@ -2,18 +2,26 @@ a , b = input().split()
 c, d = 0 , 0
 
 for i in range (len(a)) :
-    if a[i].isdigit() == True :
-        continue
-    else :
+    if a[i].isdigit() == False :
         c = i
         break
-
-for j in range (len(b)) :
-    if b[j].isdigit() == True :
-        continue
+    elif i == (len(a) - 1) :
+        a[i].isdigit() == True
+        c = i + 1
+        break
     else :
+        continue
+        
+for j in range (len(b)) :
+    if b[j].isdigit() == False :
         d = j
         break
+    elif j == (len(b) - 1) :
+        b[j].isdigit() == True
+        d = j + 1
+        break
+    else :
+        continue
 
 e = a[:c]
 f = b[:d]
