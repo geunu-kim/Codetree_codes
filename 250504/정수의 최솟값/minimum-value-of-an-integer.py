5fea77fb3999426281469dcb6462cc228c55d827
geunu-kim <splendid0904@hufs.ac.kr>
2025-05-04 17:04:00 +0900

    Title: 정수의 최솟값, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250504/정수의 최솟값/minimum-value-of-an-integer.py
@@ -0,0 +1,11 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+
+def minimum(a,b,c) :
+    minimum_2 = min(a,b,c)
+    return minimum_2
+
+
+minimum_3 = minimum(a,b,c)
+print(minimum_3)
