a1f3d2c03ad972971521b95be8d7a7f5070c5073
geunu-kim <splendid0904@hufs.ac.kr>
2025-05-13 19:30:29 +0900

    Title: 1이 되는 순간까지, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250513/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 250513/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -5,13 +5,15 @@ tot = 0
 
 def f(x) :
     global tot
-    if x % 2 == 0 :
+    if x == 1 :
+        return tot
+    elif x % 2 == 0 :
         x //= 2
         tot += 1
         if x == 1 :
             return tot
         return f(x)
-    else :
+    elif x % 2 == 1 :
         x //= 3
         tot += 1
         if x == 1 :
