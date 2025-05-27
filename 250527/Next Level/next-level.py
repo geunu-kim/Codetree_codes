bffe6cb67bb91e94a3c1b1d4c5ac9ab921ac39d2
geunu-kim <splendid0904@hufs.ac.kr>
2025-05-27 15:58:11 +0900

    Title: Next Level, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250527/Next Level/next-level.py
@@ -0,0 +1,18 @@
+user2_id, user2_level = input().split()
+user2_level = int(user2_level)
+
+# Please write your code here.
+class Next :
+    def __init__ (self,a = "codetree",b="10") :
+        self.id = a
+        self.lv = b
+
+
+Next1 = Next()
+print("user",Next1.id,"lv",Next1.lv)
+
+Next2 = Next("hello","28")
+print("user",Next2.id,"lv",Next2.lv)
+
+
+
