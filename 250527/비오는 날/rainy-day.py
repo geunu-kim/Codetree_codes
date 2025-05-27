ce60640644a3818e164e3dc10238812cf7ccc1a5
geunu-kim <splendid0904@hufs.ac.kr>
2025-05-27 16:48:19 +0900

    Title: 비오는 날, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250527/비오는 날/rainy-day.py
@@ -0,0 +1,34 @@
+n = int(input())
+date = []
+day = []
+weather = []
+cnt = [] ; cnt_2 = []
+
+for _ in range(n) :
+    d, dy, w = input().split()
+    date.append(d)
+    day.append(dy)
+    weather.append(w)
+
+# Please write your code here.
+class Future :
+    def __init__ (self,a,b,c) :
+        self.d = a
+        self.dy = b
+        self.w = c
+
+for elem in weather :
+    if elem == "Rain" :
+        cnt.append(weather.index(elem))
+    else :
+        continue
+
+for elem in cnt :
+    cnt_2.append(date[elem])
+
+sorted_cnt_2 = sorted(cnt_2) ; really = cnt_2.index(sorted_cnt_2[0])
+really_2 =  date.index(cnt_2[really])
+
+Future_2 = Future(date[really_2],day[really_2],weather[really_2])
+
+print(Future_2.d,Future_2.dy,Future_2.w)
