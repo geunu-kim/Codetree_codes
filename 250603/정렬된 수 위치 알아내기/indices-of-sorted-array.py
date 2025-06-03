373f6e4173fd0c14458ca723b04245c3338764fe
geunu-kim <splendid0904@hufs.ac.kr>
2025-06-03 20:27:22 +0900

    Title: 정렬된 수 위치 알아내기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250603/정렬된 수 위치 알아내기/indices-of-sorted-array.py
@@ -0,0 +1,19 @@
+n = int(input())
+sequence = list(map(int, input().split()))
+sequence_3 = sequence[:]
+# Please write your code here.
+sequence.sort()
+
+sequence_2 = [] ; num = 1
+
+for elem in sequence :
+    sequence_2.append((num, elem))
+    num += 1
+
+for elem in sequence_3 :
+    for i, item in enumerate(sequence_2) :
+        if elem == item[1] :
+            print(item[0], end=" ")
+            del sequence_2[i]
+        else :
+            continue
