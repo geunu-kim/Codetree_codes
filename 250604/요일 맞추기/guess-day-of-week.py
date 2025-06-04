a98ae8ca838f7d1ee3732c3d1d1bdfbf409f4ddc
geunu-kim <splendid0904@hufs.ac.kr>
2025-06-04 20:03:52 +0900

    Title: 요일 맞추기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250604/요일 맞추기/guess-day-of-week.py
+++ 250604/요일 맞추기/guess-day-of-week.py
@@ -3,7 +3,14 @@ tot_1 , tot_2 , t =  0 , 0 , 0
 # Please write your code here.
 months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day = ["Mon" , "Tue" , "Wed" , "Thu" , "Fri" , "Sat" , "Sun"]
-tot_1 = months[m1] + d1 ; tot_2 = months[m2] + d2
+
+for i in range (m1) :
+    tot_1 += months[m1]
+tot_1 += d1
+
+for i in range (m1) :
+    tot_2 += months[m2]
+tot_2 += d2
 
 if tot_2 >= tot_1 :
     t = tot_2 - tot_1
