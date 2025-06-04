28559290058d1d048d018389c591c053ca11eec0
geunu-kim <splendid0904@hufs.ac.kr>
2025-06-04 20:35:53 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250604/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,42 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input() ; tot = 0 ; tot_1 , tot_2 = 0 , 0
+
+# Please write your code here.
+months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = ["Mon" , "Tue" , "Wed" , "Thu" , "Fri" , "Sat" , "Sun"]
+B = day.index(A)
+
+for i in range (m1) :
+    tot_1 += months[i]
+tot_1 += d1
+
+for i in range (m2) :
+    tot_2 += months[i]
+tot_2 += d2
+
+if tot_2 >= tot_1 :
+    t = tot_2 - tot_1
+    while True :
+        t = t - 7
+        tot += 1
+        if t < 7 :
+            if t == B :
+                tot += 1
+                print(tot)
+                break
+            else :
+                print(tot)
+                break
+else :
+    t = tot_1 - tot_2
+    while True :
+        t = t - 7
+        tot += 1
+        if t < 7 :
+            if t == B :
+                tot += 1
+                print(tot)
+                break
+            else :
+                print(tot)
+                break
