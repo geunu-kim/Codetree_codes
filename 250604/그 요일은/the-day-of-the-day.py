f1236edb62cc7ece6edfdcb716b5b52af2f6ea92
geunu-kim <splendid0904@hufs.ac.kr>
2025-06-04 20:58:36 +0900

    Title: 그 요일은, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250604/그 요일은/the-day-of-the-day.py
+++ 250604/그 요일은/the-day-of-the-day.py
@@ -9,22 +9,15 @@ B = day.index(A)
 for i in range (m1) :
     tot_1 += months[i]
 tot_1 += d1
-print(tot_1)
 
 for i in range (m2) :
     tot_2 += months[i]
 tot_2 += d2
-print(tot_2)
 
 t = tot_2 - tot_1
-while True :
-    t = t - 7
-    tot += 1
-    if t < 7 :
-        if t == B :
-            tot += 1
-            print(tot)
-            break
-        else :
-            print(tot)
-            break
+
+for i in range(t + 1):  # 시작일부터 끝일까지 포함
+    if (B + i) % 7 == 0:
+        tot += 1
+
+print(tot)
