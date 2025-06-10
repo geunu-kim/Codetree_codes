4e3f17bff0a8f0bc9b0816d71aa2d4f9d32ed9ce
geunu-kim <splendid0904@hufs.ac.kr>
2025-06-10 23:30:50 +0900

    Title: 최대로 겹치는 지점, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250610/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 250610/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -4,7 +4,7 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 arr = [0] * 9999
 for start , end in segments :
-    for j in (start , end) :
+    for j in range (start , end + 1) :
         arr[j] += 1
 
 max_arr = max(arr)
