1e466d5430c249b86b73a4dd9e6c16236159d732
heygeunu <splendid0904@hufs.ac.kr>
2025-09-03 10:22:13 +0900

    Title: 점수 입출력, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250903/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.print("Your score is ");
+        System.out.print("143 ");
+        System.out.print("point.");
+    }
+}
