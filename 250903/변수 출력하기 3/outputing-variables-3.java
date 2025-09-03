19b93c3433fecf147fdeac9e3f904f81fce53927
heygeunu <splendid0904@hufs.ac.kr>
2025-09-03 10:00:08 +0900

    Title: 변수 출력하기 3, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250903/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+        System.out.print(a);
+        System.out.print("->");
+        System.out.print(b);
+        System.out.print("->");
+        System.out.print(c);
+    }
+}
